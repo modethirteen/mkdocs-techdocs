db074dfb82ecbdc80de6c042d6da66195518bf0e
Andy Vaughn <andy.vaughn@appfolio.com>
2024-12-20 11:57:36 -0800

    Add page meta from `.meta.yml` in current or parent folder

--- modethirteen/mkdocs/metadata/__init__.py
+++ modethirteen/mkdocs/metadata/__init__.py
@@ -1,12 +1,37 @@
-import json
-
 from mkdocs.plugins import BasePlugin
+import json
+import yaml
+import os
 
 class TechDocsMetadataPlugin(BasePlugin):
   def __init__(self):
     self.data = []
 
-  def on_page_markdown(self, markdown, page, config, **kwargs):
+  def on_pre_page(self, page, **kwargs):
+    file_path = page.file.abs_src_path
+    file_dir = os.path.dirname(file_path)
+
+    # traverse up the directory tree to locate and merge .meta.yml files
+    merged_meta = {}
+    current_dir = file_dir
+    while current_dir:
+      meta_file_path = os.path.join(current_dir, ".meta.yml")
+      if os.path.isfile(meta_file_path):
+        with open(meta_file_path, "r") as meta_file:
+          meta_data = yaml.safe_load(meta_file)
+          if meta_data:
+            merged_meta.update(meta_data)
+      parent_dir = os.path.dirname(current_dir)
+      if parent_dir == current_dir:
+        break
+      current_dir = parent_dir
+
+    # merge the metadata into the page's frontmatter
+    if merged_meta:
+      page_meta = page.meta or {}
+      page.meta = {**merged_meta, **page_meta}
+
+  def on_page_markdown(self, markdown, page, **kwargs):
     if page.meta:
       self.data.append({ "url": page.file.url, "meta": page.meta })
     return markdown
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,6 +1,7 @@
 import json
 import os
 import tempfile
+import logging
 from unittest.mock import Mock
 
 import pytest
@@ -63,9 +64,9 @@ def test_it_can_store_page_frontmatter_metadata_in_techdocs_metadata(mock_config
   baz.read_source(config=mock_config)
 
   # act
-  plugin_instance.on_page_markdown(page=foo, config=mock_config, markdown="markdown")
-  plugin_instance.on_page_markdown(page=bar, config=mock_config, markdown="markdown")
-  plugin_instance.on_page_markdown(page=baz, config=mock_config, markdown="markdown")
+  plugin_instance.on_page_markdown(page=foo, markdown="markdown")
+  plugin_instance.on_page_markdown(page=bar, markdown="markdown")
+  plugin_instance.on_page_markdown(page=baz, markdown="markdown")
   plugin_instance.on_post_build(config=mock_config)
 
   # assert
@@ -78,6 +79,68 @@ def test_it_can_store_page_frontmatter_metadata_in_techdocs_metadata(mock_config
       { "url": "foo/", "meta": { "key1": "value1", "key2": "value2" }},
       { "url": "baz/", "meta": { "key3": "value3", "key4": "value4" }}
     ]
+
+def test_it_can_merge_metadata_file_content(mock_config, plugin_instance):
+
+  # arrange
+  docs_dir = mock_config["docs_dir"]
+  site_dir = mock_config["site_dir"]
+  os.mkdir(os.path.join(docs_dir, "xyzzy"))
+  foo = Page(
+    "foo",
+    File("xyzzy/foo.md", src_dir=docs_dir, dest_dir=site_dir, use_directory_urls=True),
+    config={},
+  )
+  bar = Page(
+    "bar",
+    File("bar.md", src_dir=docs_dir, dest_dir=site_dir, use_directory_urls=True),
+    config={},
+  )
+  os.mkdir(os.path.join(docs_dir, "foobar"))
+  baz = Page(
+    "baz",
+    File("foobar/baz.md", src_dir=docs_dir, dest_dir=site_dir, use_directory_urls=True),
+    config={},
+  )
+  plugin_instance.log = Mock()
+  with open(os.path.join(docs_dir, ".meta.yml"), "w", encoding="utf-8") as f:
+    f.write("description: bazz")
+  with open(os.path.join(docs_dir, "xyzzy", ".meta.yml"), "w", encoding="utf-8") as f:
+    f.write("type: how-to\ntags:\n- plugh")
+  with open(os.path.join(docs_dir, "foobar", ".meta.yml"), "w", encoding="utf-8") as f:
+    f.write("tags:\n- typescript\ntype: reference")
+
+  with open(os.path.join(docs_dir, "xyzzy", "foo.md"), "w", encoding="utf-8") as f:
+    f.write("# foo")
+  foo.read_source(config=mock_config)
+  with open(os.path.join(docs_dir, "bar.md"), "w", encoding="utf-8") as f:
+    f.write("---\nkey3: value3\nkey4: value4\n---\n# bar")
+  bar.read_source(config=mock_config)
+  with open(os.path.join(docs_dir, "foobar", "baz.md"), "w", encoding="utf-8") as f:
+    f.write("---\ntags:\n- php\n---\n# baz")
+  baz.read_source(config=mock_config)
+
+  # act
+  plugin_instance.on_pre_page(page=foo)
+  plugin_instance.on_pre_page(page=bar)
+  plugin_instance.on_pre_page(page=baz)
+
+  # assert
+  assert foo.meta == {
+    "type": "how-to",
+    "tags": ["plugh"],
+    "description": "bazz"
+  }
+  assert bar.meta == {
+    "key3": "value3",
+    "key4": "value4",
+    "description": "bazz"
+  }
+  assert baz.meta == {
+    "type": "reference",
+    "tags": ["php"],
+    "description": "bazz"
+  }
 
 if __name__ == "__main__":
   pytest.main()
